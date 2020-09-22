f6e9449445365321b3bba679cf6572f8ef7bcb3a
Haato3o <matheus_mrp@hotmail.com>
2020-09-22 14:32:14 -0300

    test update

---
+++ pack.py
@@ -0,0 +1,16 @@
+import json
+import os
+import pathlib
+import hashlib
+
+# Put the files you want to move to the binaries directory here
+filesToMove = [
+    "PluginAutoUpdateTest.dll",
+    "module.json"
+]
+
+relativePath = "PluginAutoUpdateTest\\bin\\x64\\Release"
+
+for f in os.listdir(relativePath):
+    if (f.replace(relativePath, "") in filesToMove):
+        os.rename(f"{relativePath}\\{f}", f"binaries\\{f.replace(relativePath, '')}")
