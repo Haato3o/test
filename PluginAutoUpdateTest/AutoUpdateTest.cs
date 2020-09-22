e7f37b03d7b9f1c8854fdc4a1fab036b0dcd900a
Haato3o <matheus_mrp@hotmail.com>
2020-09-22 13:54:26 -0300

    aaaaa

---
+++ PluginAutoUpdateTest/AutoUpdateTest.cs
@@ -0,0 +1,32 @@
+﻿using HunterPie.Core;
+using HunterPie.Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HunterPie.Plugins
+{
+    public class AutoUpdateTest : IPlugin
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Game Context { get; set; }
+
+        public void Initialize(Game context)
+        {
+            Name = "AutoUpdateTest";
+            Description = "Woah";
+
+            Context = context;
+
+            this.Log("Hello world!");
+        }
+
+        public void Unload()
+        {
+            
+        }
+    }
+}
